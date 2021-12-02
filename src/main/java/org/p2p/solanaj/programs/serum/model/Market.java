222d617bed6920d9d6331eb7589a22d44208af8f
makarid <makarid@gmail.com>
2021-12-03 01:08:28 +0200

    CleanUps

--- src/main/java/org/p2p/solanaj/programs/serum/model/Market.java
+++ src/main/java/org/p2p/solanaj/programs/serum/model/Market.java
@@ -1,8 +1,14 @@
 package org.p2p.solanaj.programs.serum.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.p2p.solanaj.core.PublicKey;
 
 /** Class to represent a Serum market. Should be instantiated using a {@link .MarketBuilder}. */
+@ToString
+@Getter
+@Setter
 public class Market {
 
   private AccountFlags accountFlags;
@@ -37,46 +43,6 @@ public class Market {
   // Data from token mints
   private byte baseDecimals;
   private byte quoteDecimals;
-
-  public byte getBaseDecimals() {
-    return baseDecimals;
-  }
-
-  public void setBaseDecimals(byte baseDecimals) {
-    this.baseDecimals = baseDecimals;
-  }
-
-  public byte getQuoteDecimals() {
-    return quoteDecimals;
-  }
-
-  public void setQuoteDecimals(byte quoteDecimals) {
-    this.quoteDecimals = quoteDecimals;
-  }
-
-  public EventQueue getEventQueue() {
-    return eventQueue;
-  }
-
-  public void setEventQueue(EventQueue eventQueue) {
-    this.eventQueue = eventQueue;
-  }
-
-  public OrderBook getBidOrderBook() {
-    return bidOrderBook;
-  }
-
-  public void setBidOrderBook(OrderBook bidOrderBook) {
-    this.bidOrderBook = bidOrderBook;
-  }
-
-  public OrderBook getAskOrderBook() {
-    return askOrderBook;
-  }
-
-  public void setAskOrderBook(OrderBook askOrderBook) {
-    this.askOrderBook = askOrderBook;
-  }
 
   public static Market readMarket(byte[] data) {
     Market market = new Market();
@@ -144,166 +110,6 @@ public class Market {
     return market;
   }
 
-  public AccountFlags getAccountFlags() {
-    return accountFlags;
-  }
-
-  public void setAccountFlags(AccountFlags accountFlags) {
-    this.accountFlags = accountFlags;
-  }
-
-  public PublicKey getOwnAddress() {
-    return ownAddress;
-  }
-
-  public void setOwnAddress(PublicKey ownAddress) {
-    this.ownAddress = ownAddress;
-  }
-
-  public long getVaultSignerNonce() {
-    return vaultSignerNonce;
-  }
-
-  public void setVaultSignerNonce(long vaultSignerNonce) {
-    this.vaultSignerNonce = vaultSignerNonce;
-  }
-
-  public PublicKey getBaseMint() {
-    return baseMint;
-  }
-
-  public void setBaseMint(PublicKey baseMint) {
-    this.baseMint = baseMint;
-  }
-
-  public PublicKey getQuoteMint() {
-    return quoteMint;
-  }
-
-  public void setQuoteMint(PublicKey quoteMint) {
-    this.quoteMint = quoteMint;
-  }
-
-  public PublicKey getBaseVault() {
-    return baseVault;
-  }
-
-  public void setBaseVault(PublicKey baseVault) {
-    this.baseVault = baseVault;
-  }
-
-  public long getBaseDepositsTotal() {
-    return baseDepositsTotal;
-  }
-
-  public void setBaseDepositsTotal(long baseDepositsTotal) {
-    this.baseDepositsTotal = baseDepositsTotal;
-  }
-
-  public long getBaseFeesAccrued() {
-    return baseFeesAccrued;
-  }
-
-  public void setBaseFeesAccrued(long baseFeesAccrued) {
-    this.baseFeesAccrued = baseFeesAccrued;
-  }
-
-  public PublicKey getQuoteVault() {
-    return quoteVault;
-  }
-
-  public void setQuoteVault(PublicKey quoteVault) {
-    this.quoteVault = quoteVault;
-  }
-
-  public long getQuoteDepositsTotal() {
-    return quoteDepositsTotal;
-  }
-
-  public void setQuoteDepositsTotal(long quoteDepositsTotal) {
-    this.quoteDepositsTotal = quoteDepositsTotal;
-  }
-
-  public long getQuoteFeesAccrued() {
-    return quoteFeesAccrued;
-  }
-
-  public void setQuoteFeesAccrued(long quoteFeesAccrued) {
-    this.quoteFeesAccrued = quoteFeesAccrued;
-  }
-
-  public long getQuoteDustThreshold() {
-    return quoteDustThreshold;
-  }
-
-  public void setQuoteDustThreshold(long quoteDustThreshold) {
-    this.quoteDustThreshold = quoteDustThreshold;
-  }
-
-  public PublicKey getRequestQueue() {
-    return requestQueue;
-  }
-
-  public void setRequestQueue(PublicKey requestQueue) {
-    this.requestQueue = requestQueue;
-  }
-
-  public PublicKey getEventQueueKey() {
-    return eventQueueKey;
-  }
-
-  public void setEventQueueKey(PublicKey eventQueueKey) {
-    this.eventQueueKey = eventQueueKey;
-  }
-
-  public PublicKey getBids() {
-    return bids;
-  }
-
-  public void setBids(PublicKey bids) {
-    this.bids = bids;
-  }
-
-  public PublicKey getAsks() {
-    return asks;
-  }
-
-  public void setAsks(PublicKey asks) {
-    this.asks = asks;
-  }
-
-  public long getBaseLotSize() {
-    return baseLotSize;
-  }
-
-  public void setBaseLotSize(long baseLotSize) {
-    this.baseLotSize = baseLotSize;
-  }
-
-  public long getQuoteLotSize() {
-    return quoteLotSize;
-  }
-
-  public void setQuoteLotSize(long quoteLotSize) {
-    this.quoteLotSize = quoteLotSize;
-  }
-
-  public long getFeeRateBps() {
-    return feeRateBps;
-  }
-
-  public void setFeeRateBps(long feeRateBps) {
-    this.feeRateBps = feeRateBps;
-  }
-
-  public long getReferrerRebatesAccrued() {
-    return referrerRebatesAccrued;
-  }
-
-  public void setReferrerRebatesAccrued(long referrerRebatesAccrued) {
-    this.referrerRebatesAccrued = referrerRebatesAccrued;
-  }
-
   // TODO - implement all of these, just updating bids, asks, and event queue.
   // However, most of these fields won't change after a reload.
   public void reload(MarketBuilder builder) {
@@ -311,59 +117,5 @@ public class Market {
     this.bidOrderBook = market.getBidOrderBook();
     this.askOrderBook = market.getAskOrderBook();
     this.eventQueue = market.getEventQueue();
-  }
-
-  @Override
-  public String toString() {
-    return "Market{"
-        + "accountFlags="
-        + accountFlags
-        + ", ownAddress="
-        + ownAddress
-        + ", vaultSignerNonce="
-        + vaultSignerNonce
-        + ", baseMint="
-        + baseMint
-        + ", quoteMint="
-        + quoteMint
-        + ", baseVault="
-        + baseVault
-        + ", baseDepositsTotal="
-        + baseDepositsTotal
-        + ", baseFeesAccrued="
-        + baseFeesAccrued
-        + ", quoteVault="
-        + quoteVault
-        + ", quoteDepositsTotal="
-        + quoteDepositsTotal
-        + ", quoteFeesAccrued="
-        + quoteFeesAccrued
-        + ", quoteDustThreshold="
-        + quoteDustThreshold
-        + ", requestQueue="
-        + requestQueue
-        + ", eventQueue="
-        + eventQueueKey
-        + ", bids="
-        + bids
-        + ", asks="
-        + asks
-        + ", baseLotSize="
-        + baseLotSize
-        + ", quoteLotSize="
-        + quoteLotSize
-        + ", feeRateBps="
-        + feeRateBps
-        + ", referrerRebatesAccrued="
-        + referrerRebatesAccrued
-        + ", bidOrderBook="
-        + bidOrderBook
-        + ", askOrderBook="
-        + askOrderBook
-        + ", baseDecimals="
-        + baseDecimals
-        + ", quoteDecimals="
-        + quoteDecimals
-        + '}';
   }
 }
