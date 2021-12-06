22a0dd7ff0bd41d549598c4d9b1e13159b853637
makarid <makarid@gmail.com>
2021-12-06 20:06:25 +0200

    Adding default encoding base64 on ProgramAccountConfig

--- src/main/java/org/p2p/solanaj/rpc/types/config/ProgramAccountConfig.java
+++ src/main/java/org/p2p/solanaj/rpc/types/config/ProgramAccountConfig.java
@@ -6,17 +6,17 @@ import org.p2p.solanaj.rpc.types.config.RpcSendTransactionConfig.Encoding;
 
 public class ProgramAccountConfig {
 
-    private Encoding encoding = null;
+  private Encoding encoding = Encoding.base64;
 
-    private List<Object> filters = null;
+  private List<Object> filters = null;
 
-    private String commitment = "processed";
+  private String commitment = "processed";
 
-    public ProgramAccountConfig(List<Object> filters) {
-        this.filters = filters;
-    }
+  public ProgramAccountConfig(List<Object> filters) {
+    this.filters = filters;
+  }
 
-    public ProgramAccountConfig(Encoding encoding) {
-        this.encoding = encoding;
-    }
+  public ProgramAccountConfig(Encoding encoding) {
+    this.encoding = encoding;
+  }
 }
